5c87669d6d5b79ff948d2c407e052aa221e228a5
Richard Andrew <richard.andrewdata@gmail.com>
2020-02-06 16:35:47 +0200

    Add comments

--- BZ_ZONES_counting.py
+++ BZ_ZONES_counting.py
@@ -7,12 +7,19 @@ import time
 import os
 #--------------------------------------------------------
 
+# check to see if the fortran to python library exits
+# If not run f2py compiling thr openMP fortran file into a python library
+
 if (not os.path.exists('ws.cpython-37m-darwin.so')):
     os.system("f2py3.7 -c -m ws bzones_lib_counting.f90 --f90flags=-fopenmp -lgomp")
 
+# Import this library 
 import ws
 
-# Rvect generator
+# NOTE 'ws.cpython-37m-darwin.so' is the library and 'ws' is the import name
+
+#----------------------------------------------
+# Gvect generator function
 def gvect(gs,gcut,n):
     # define spanning vectors
     g = np.zeros(((n+2)*(n+2)*(n+2)*8*8*8,3))
@@ -45,6 +52,7 @@ def gvect(gs,gcut,n):
 PI = np.pi
 #### Enter in parameters
 # Enter crystal system type
+
 print ('Enter cystal system type:')
 print ()
 print ('1-CUBIC')
@@ -58,6 +66,7 @@ print ()
 crys = int(input('Enter: '))
 
 # Enter crystal type and associated parameters
+
 ca=1.0
 ba=1.0
 if(crys==1):
@@ -96,11 +105,12 @@ else:
 print ()
 
 # RS are the spanning vectors
-# R are the generated lattice vectors
+
 RS = np.zeros(shape=(3,3))
 
 
 # Set the spanning vectors in units of 'a'
+
 if (crys==1 and s==1):
     RS[0,0] = 1.0
     RS[0,1] = 0.0
@@ -287,10 +297,14 @@ print ('r2= ',RS[1,0], RS[1,1], RS[1,2])
 print ('r3= ',RS[2,0], RS[2,1], RS[2,2])
 print
 
-GS = np.zeros(shape=(3,3))
 # Generate reciprocal spanning vectors in units of 2pi/a
+
+GS = np.zeros(shape=(3,3))
+
 V = np.abs(RS[0,0]*(RS[1,1]*RS[2,2] - RS[2,1]*RS[1,2])+RS[0,1]*(RS[2,0]*RS[1,2] - RS[1,0]*RS[2,2]) + RS[0,2]*(RS[1,0]*RS[2,1] - RS[2,0]*RS[1,1]))
+
 print ('Unit cell volume= ',V)
+
 GS[0,0] = (RS[0,1]*RS[1,2] - RS[1,1]*RS[0,2])/V
 GS[0,1] = (RS[1,0]*RS[0,2] - RS[1,2]*RS[0,0])/V
 GS[0,2] = (RS[0,0]*RS[1,1] - RS[1,0]*RS[0,1])/V
@@ -309,53 +323,67 @@ print ('b3= ',GS[2,0], GS[2,1], GS[2,2])
 print ()
 
 # set cutoff radius squared and generate Gvect
+
 GC = int(input('Enter Gcut^2  '))
 
 # define reciprocal lattice spanning vectors
-# here it is FCC
 
 t1=time.time()
+
 # estimate number of first spanning vector from gamma to cutoff radius
 # and allocate and initialize G vectors:generate ordered G vectors
+
 GGS1 = GS[0,0]**2 + GS[0,1]**2 + GS[0,2]**2
 GGS2 = GS[1,0]**2 + GS[1,1]**2 + GS[1,2]**2
 GGS3 = GS[2,0]**2 + GS[2,1]**2 + GS[2,2]**2
 GGS = min(GGS1,GGS2,GGS3)
+
 nr=int(np.sqrt(GC/GGS)) + 1
-G = gvect(GS,GC,nr)
+
+G = gvect(GS,GC,nr) # generate G vector with length squared less than Gcut^2
+
 ll=len(G)
 print("length G ",ll)
-# save G vectors to a file
-#gdata = open('G_VECTORS.dat','w')
-#for i in range(ll):
-#    print >> gdata, np.linalg.norm(G[i])**2, G[i]
-#gdata.close()
 
 # Define GRID based on parallel-piped based on b1,b2,b3
+
 m1 = int(input('enter how many multiples of G1 '))
 m2 = int(input('enter how many multiples of G2 '))
 m3 = int(input('enter how many multiples of G3 '))
 N1 = int(input('Enter number of grid points along 1 direction '))
 N2 = int(input('Enter number of grid points along 2 direction '))
 N3 = int(input('Enter number of grid points along 3 direction '))
-BZ = int(input('Enter BZ '))
+
+# choose bz
+
+BZ = int(input('Enter BZ ')) 
+
 # open files to store BZ data
+
 if (os.path.exists('BZ_ZONE.dat')):
     os.system("rm -f BZ_ZONE.dat")
 
-# call procedure to create BZ's
+# call fortran procedure through ws library to create k-points within bz
+# Results saved to a datafile
+
 ws.gen_kp(m1,m2,m3,N1,N2,N3,BZ,GS,G,ll)
+
 print ()
 t2=time.time()
 print ('time: ','%E' %(t2-t1), 'sec')
 
+# read k-points into a DataFrame
+
 out_df = pd.read_csv("./BZ_ZONE.dat",header=None,delim_whitespace=True)
-os.system("rm -f BZ_ZONE.dat")
+os.system("rm -f BZ_ZONE.dat") # delete k-point datafile
 print('file read')
 out_df.columns = ['x','y','z','radius','count']
 print(out_df.head())
 
 print(out_df.describe())
+
+# import visualisation library
+
 from mayavi import mlab
 
 x = out_df[['x']].values
@@ -363,7 +391,12 @@ y = out_df[['y']].values
 z = out_df[['z']].values
 rad = out_df[['radius']].values
 
+# create scatter pipeline with x,y,z and radius to define depth colors
+
 src = mlab.pipeline.scalar_scatter(x, y, z, rad)
+
+# plot points in a mayavi sceen window
+
 pts = mlab.pipeline.glyph(src, scale_mode='none',scale_factor=.1,colormap="winter")
 
 mlab.show()
